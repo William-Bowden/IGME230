76c9570d3de31776de912555d8b0d1fd7eaf0e59
William Bowden (RIT Student) <wjb5377@ad.rit.edu>
2018-04-23 18:43:48 -0400

    integrated it into the site

--- project3/site/js/classes.js
+++ project3/site/js/classes.js
@@ -1,26 +1,70 @@
-class Player extends PIXI.Sprite{
-	constructor(x = 0, y = 0){
-		super(PIXI.loader.resources["images/Spaceship.png"].texture);
-		this.anchor.set(0.5,0.5);
-		this.scale.set(0.1);
+class Avatar extends PIXI.Graphics{
+	constructor(radius=10, color=0xFF0000, x=0, y=0,speed=5){
+		super();
+		this.radius = radius;
 		this.x = x;
 		this.y = y;
+		this.speed = speed;
+		this.beginFill(color);
+		this.lineStyle(2,0x000000,1);
+		this.drawCircle(0,0,radius);
+		this.endFill();
+		
+		// other properties
+		this.isGrounded = false;
+		this.ignorePlatforms = false;
+		this.dx = 0; // per second
+		this.dy = 0; // per second
 	}
 	
-	function moveup() {
-		this.y -= 1; 
-	}
+	update(dt, sceneWidth, sceneHeight){
+		
 
-	function movedown() {
-		this.y += 1; 
-	}
+		this.x += this.dx * dt;
+		this.y += this.dy * dt;
 
-	function moveleft() {
-		this.x -= 1;
+		this.x = clamp(this.x, this.radius, sceneWidth - this.radius);
+		this.y = clamp(this.y, this.radius, sceneHeight - this.radius);
+		
+		// wall bounce
+		if( this.x == sceneWidth - this.radius || this.x == this.radius ){
+			this.dx = -this.dx;
+		}
+		
+		// floor/ceiling bounce
+		if( this.y == sceneHeight - this.radius || this.y == this.radius ){
+			this.dy = -this.dy/2;
+			
+			if(this.isGrounded){
+			   this.dy = 0;
+		   	}
+			
+		}
+		else{
+			this.dy += 9.81;
+		}
+		
+		
+		// jumping
+		if(this.dy == 0 || this.y == sceneHeight - this.radius){
+			this.isGrounded = true;
+		}
+		else{
+			this.isGrounded = false;
+		}
+		
+		
 	}
+}
 
-	function moveright() {
-		this.x += 1;
+class Platform extends PIXI.Graphics{
+	constructor(width=100, height=10, color=0x476CAD, x=0, y=0){
+		super();
+		this.x = x;
+		this.y = y;
+		this.beginFill(color);
+		this.lineStyle(2,0x000000,1);
+		this.drawRect(x, y, width, height);
+		this.endFill();
 	}
-	
 }
--- project3/site/js/main.js
+++ project3/site/js/main.js
@@ -1,225 +1,86 @@
-// We will use `strict mode`, which helps us by having the browser catch many common JS mistakes
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-"use strict";
-const app = new PIXI.Application(600,600);
-document.querySelector("#gameWindow").appendChild(app.view);
-
-// constants
-const sceneWidth = app.view.width;
-const sceneHeight = app.view.height;	
-
-// pre-load the images
-PIXI.loader.
-add(["images/Spaceship.png","images/explosions.png"]).
-on("progress",e=>{console.log(`progress=${e.progress}`)}).
-load(setup);
-
-// aliases
-let stage;
-
-// game variables
-let startScene;
-let gameScene, player, scoreLabel, gameOverScoreLabel, lifeLabel, levelNum;
-let gameOverScene;
-
-let score = 0;
-let life = 100;
-let paused = true;
-
-function setup() {
-	stage = app.stage;
-// #1 - Create the `start` scene
-	startScene = new PIXI.Container();
-    stage.addChild(startScene);
-    
-// #2 - Create the main `game` scene and make it invisible
-    gameScene = new PIXI.Container();
-    gameScene.visible = false;
-    stage.addChild(gameScene);
-    
-// #3 - Create the `gameOver` scene and make it invisible
-	gameOverScene = new PIXI.Container();
-    gameOverScene.visible = false;
-    stage.addChild(gameOverScene);
-    
-// #4 - Create labels for all 3 scenes
-	createLabelsAndButtons();
-    
-// #5 - Create character
-	player = new Player();
-	gameScene.addChild(player);
+const app = new PIXI.Application(600,500);
+document.querySelector("#gameWindow").appendChild(app.view); 
+let player;
 	
-// #8 - Start update loop
-	app.ticker.add(gameLoop);
-}
+let platforms = [];
+let currentPlatforms = [];
 
-function createLabelsAndButtons(){
-    let buttonStyle = new PIXI.TextStyle({
-        fill: 0xFF0000,
-        fontSize: 48,
-        fontFamily: "Verdana"
-        
-    });
-    
-    // 1 set up 'startScene'
-    // 1A - make the top start label
-    let startLabel1 = new PIXI.Text("JumpyMan");
-    startLabel1.style = new PIXI.TextStyle({
-        fill: 0xFFFFFF,
-        fontSize: 84,
-        fontFamily: "Verdana",
-        stroke: 0xFF0000,
-        strokeThickness: 6
-    });
-    startLabel1.x = 50;
-    startLabel1.y = 120;
-    startScene.addChild(startLabel1);
-    
-    //1B - make the middle start label
-    let startLabel2 = new PIXI.Text("Do you even jump?");
-    startLabel2.style = new PIXI.TextStyle({
-        fill: 0xFFFFFF,
-        fontSize: 28,
-        fontFamily: "Verdana",
-        fontStyle: "italic",
-        stroke: 0xFF0000,
-        strokeThickness: 6
-    });
-    startLabel2.x = 185;
-    startLabel2.y = 300;
-    startScene.addChild(startLabel2);
-    
-    //1C - make the start game button
-    let startButton = new PIXI.Text("Start!");
-    startButton.style = buttonStyle;
-    startButton.x = 225;
-    startButton.y = sceneHeight - 100;
-    startButton.interactive = true;
-    startButton.buttonMode = true;
-    startButton.on("pointerup", startGame);
-    startButton.on("pointerover", e => e.target.alpha = 0.7);
-    startButton.on("pointerout", e => e.currentTarget.alpha = 1.0);
-    startScene.addChild(startButton);
-    
-    // 2 - set up 'gameScene'
-    let textStyle = new PIXI.TextStyle({
-        fill: 0xFFFFFF,
-        fontSize: 18,
-        fontFamily: "Verdana",
-        stroke: 0xFF0000,
-        strokeThickness: 4
-    });
-    
-    // 2A - make score label
-    scoreLabel = new PIXI.Text();
-    scoreLabel.style = textStyle;
-    scoreLabel.x = 5;
-    scoreLabel.y = 5;
-    gameScene.addChild(scoreLabel);
-    
-    // 2B - make life label
-    lifeLabel = new PIXI.Text();
-    lifeLabel.style = textStyle;
-    lifeLabel.x = 5;
-    lifeLabel.y = 26;
-    gameScene.addChild(lifeLabel);
-    
-    
-    // 3 - set up 'gameOverScene'
-    // 3A - make game over text
-    let gameOverText = new PIXI.Text("Game Over\n      :-O");
-    textStyle = new PIXI.TextStyle({
-        fill: 0xFFFFFF,
-        fontSize: 64,
-        fontFamily: "Verdana",
-        stroke: 0xFF0000,
-        strokeThickness: 6
-    });
-    gameOverText.style = textStyle;
-    gameOverText.x = 100;
-    gameOverText.y = sceneHeight/2 - 160;
-    gameOverScene.addChild(gameOverText);
+
+window.onload = ()=>{
+	player = new Avatar(10,0x00FF00,300,200);
+	app.stage.addChild(player);
 	
-	// 3extra - make game over score text
-    gameOverScoreLabel = new PIXI.Text("Score: ");
-    textStyle = new PIXI.TextStyle({
-        fill: 0xFFFFFF,
-        fontSize: 28,
-        fontFamily: "Verdana",
-        stroke: 0xFF0000,
-        strokeThickness: 4
-    });
-    gameOverScoreLabel.style = textStyle;
-    gameOverScoreLabel.x = sceneWidth/2 - 80;
-    gameOverScoreLabel.y = sceneHeight/2;
-    gameOverScene.addChild(gameOverScoreLabel);
-    
-    // 3B - make "play again?" button
-    let playAgainBtn = new PIXI.Text("Play Again?");
-    playAgainBtn.style = buttonStyle;
-    playAgainBtn.x = 150;
-    playAgainBtn.y = sceneHeight - 100;
-    playAgainBtn.interactive = true;
-    playAgainBtn.buttonMode = true;
-    playAgainBtn.on("pointerup", startGame);
-    playAgainBtn.on("pointerover", e => e.target.alpha = 0.7);
-    playAgainBtn.on("pointerout", e => e.currentTarget.alpha = 1.0);
-    gameOverScene.addChild(playAgainBtn);
-    
-    
-}
-
-function startGame(){
-    startScene.visible = false;
-    gameOverScene.visible = false;
-    gameScene.visible = true;
-	levelNum = 1;
-	score = 0;
-	life = 100;
-	player.x = 300;
-	player.y = 550;
-	loadLevel();
-}
-
-function gameLoop(){
-	if (paused) return;
 	
-// #1 - Calculate "delta time"
-	let deltaTime = 1/app.ticker.FPS;
-	if(deltaTime > 1/12) {
-		deltaTime = 1/12;
+	//Math.ceil((Math.random()*10) /3)
+	for(let i = 0; i < 2; i++ ){
+		newPlat = platforms[Math.floor(Math.random() * platforms.size)];
+		
+		// if that platform is already in the current platforms, try for a new platform
+		for(let j = 0; j < currentPlatforms.length; j++){
+			if(currentPlatforms[j] == newPlat){
+				console.log("Platform already in currentPlatforms");
+				i--;
+				continue;
+			}
+		}
+		// otherwise, add the new platform
+		currentPlatforms.push();
+
 	}
 	
-// #2 - Move player
-	movement = {"x": 0, "y": 0};
-    	
-	let amount = 6 * deltaTime;
+	let platform1 = new Platform(100, 10, 0x476CAD, 100, 150);
+	app.stage.addChild(platform1);
 	
-	// linear interpolation
-	let newX = lerp(player.x, mousePos.x, amount);
-	let newY = lerp(player.y, mousePos.y, amount);
+	let platform2 = new Platform(100, 10, 0x476CAD, 200, 100);
+	app.stage.addChild(platform2);
 	
-	// clamp the player to the screen
-	let w2 = player.width/2;
-	let h2 = player.height/2;
-	player.x = clamp(newX, 0 + w2, sceneWidth - w2);
-	player.y = clamp(newY, 0 + h2, sceneHeight - h2);
+	platforms.push(platform1);
+	platforms.push(platform2);
 	
-// #5 - Check for Collisions
-	
-};
+	app.renderer.backgroundColor = 0x5d5d5d;
 
+	// Animation Loop
+	app.ticker.add(()=>{
+		// #1 - Calculate "delta time"
+		let dt = 1/app.ticker.FPS;
+		if (dt > 1/12) dt=1/12;
+		
+		// #2 - Check Input
+		if(keys[keyboard.RIGHT]){
+			player.dx += player.speed;
+		}else if(keys[keyboard.LEFT]) {
+			player.dx += -player.speed;
+		}else if (player.dx != 0){
+			player.dx -= 3 * (player.dx * dt);
+			if(Math.abs(player.dx) < 1){
+			   player.dx = 0;
+			}
+		}
+		
+		if(keys[keyboard.UP] && player.isGrounded) {
+			player.dy -= clamp(player.dy, 350, 350);
+		}
+		
+		if(keys[keyboard.DOWN]) {
+			player.ignorePlatforms = true;
+		}
+		else{
+			player.ignorePlatforms = false;
+		}
+		
+		// #3 - move avatar
+		player.update(dt, app.view.width, app.view.height);
+		
+		// #4 Check collisions
+		for(let i = 0; i < platforms.length; i++){
+			if(rectsIntersect(player, platforms[i]) && !player.ignorePlatforms){
+				player.y -= 0.6;
 
-function end(){
-	paused = true;
+				player.dy = -player.dy/2;
+				player.isGrounded = true;
+			}
+		}
 	
-	gameOverScoreLabel.text = `Score ${score}`;
+	});
 	
-	gameOverScene.visible = true;
-	gameScene.visible = false;
-}
-
-function loadLevel(){
-	paused = false;
-}
-
+} // end window.onload
